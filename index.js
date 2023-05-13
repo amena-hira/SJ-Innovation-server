f6b9bac3a96c6e9fb698c5c8e8c5f5881e520140
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2023-05-13 12:40:51 +0200

    initial package setting and connect to mongodb

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.port || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cwbwt8c.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
+
+async function run() {
+    try {
+        
+    } 
+    finally{
+
+    }
+}
+
+run().catch(error => console.log(error));
+
+app.get('/', async (req, res) => {
+    res.send('server is running');
+})
+
+app.listen(port, () => console.log(`Server running on ${port}`))
